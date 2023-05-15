fbc1d1bfc51760f2e34143fd04aeebc4c44cb7a8
rjayasin <ruvanjayasinghe@gmail.com>
2023-05-15 11:21:28 -0700

    working through auth

---
+++ auth.js
@@ -0,0 +1,108 @@
+function generateRandomString(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+async function hash(string) {
+    const utf8 = new TextEncoder().encode(string);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+        .map((bytes) => bytes.toString(16).padStart(2, '0'))
+        .join('');
+    return hashHex;
+}
+
+let codeVerifier = generateRandomString(128);
+const hashed = await hash(codeVerifier);
+// const digest = await window.crypto.subtle.digest('SHA-256', hashed);
+const digest = await hash(codeVerifier);
+
+async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+        return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=+$/, '');
+    }
+
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+
+    return base64encode(digest);
+}
+
+const clientId = '201bae405c6d47c49e045f8734fc82da';
+const redirectUri = 'https://rjayasin.github.io/visualizer/spotify.html';
+// const redirectUri = 'http://127.0.0.1:8080/spotify.html';
+
+
+generateCodeChallenge(codeVerifier).then(codeChallenge => {
+    let state = generateRandomString(16);
+    let scope = 'user-read-private user-read-email';
+
+    localStorage.setItem('code_verifier', codeVerifier);
+
+    let args = new URLSearchParams({
+        response_type: 'code',
+        client_id: clientId,
+        scope: scope,
+        redirect_uri: redirectUri,
+        state: state,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge
+    });
+
+    window.location = 'https://accounts.spotify.com/authorize?' + args;
+});
+
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+codeVerifier = localStorage.getItem('code_verifier');
+
+let body = new URLSearchParams({
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: redirectUri,
+    client_id: clientId,
+    code_verifier: codeVerifier
+});
+
+const response = fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: body
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('HTTP status ' + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        localStorage.setItem('access_token', data.access_token);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+async function getProfile(accessToken) {
+    accessToken = localStorage.getItem('access_token');
+
+    const response = await fetch('https://api.spotify.com/v1/me', {
+        headers: {
+            Authorization: 'Bearer ' + accessToken
+        }
+    });
+
+    const data = await response.json();
+}
